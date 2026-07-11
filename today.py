8021e15e0a39d99da50ee1f5bbb7acf0156250e0
Nu2lan <cybersoftmmc@gmail.com>
2026-07-11 07:24:07 +0400

    Commit

--- today.py
+++ today.py
@@ -303,6 +303,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
+    justify_format(root, 'age_data', age_data, 47)
     justify_format(root, 'commit_data', commit_data, 22)
     justify_format(root, 'star_data', star_data, 14)
     justify_format(root, 'repo_data', repo_data, 6)
@@ -427,8 +428,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    # TODO: change this to your own birthday -> datetime.datetime(YEAR, MONTH, DAY)
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2000, 1, 1))
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(1998, 3, 9))
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
